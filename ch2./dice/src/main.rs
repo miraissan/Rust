24b9c4e77518b9b5cafccdeaad2fa77639929ef9
miraissan <tora1962aa@gmail.com>
2022-12-24 11:49:25 +0900

    Delete ch2. directory

--- ch2./dice/src/main.rs
+++
@@ -1,9 +0,0 @@
-use rand::Rng;
-
-fn main() {
-    let mut rng = rand::thread_rng();
-    for _ in 0..6{
-     let dice = rng.gen_range(1..=7);   
-    println!("{}",dice);
-    }
-}
--- ch2./dice/src/range.rs
+++
@@ -1,4 +0,0 @@
-fn main(){
-    let r = 3..15;
-    println!("{}..{}",r.start,r.end);
-}
