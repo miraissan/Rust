45a6aac700a352f199ab5b194586eadf70a9a21c
miraissan <tora1962aa@gmail.com>
2022-12-26 10:50:28 +0900

    作成

--- ch2/bingocard/target/debug/bingocard.d
+++
@@ -1,1 +0,0 @@
-C:\Users\SLINK-004\src\ch2\bingocard\target\debug\bingocard.exe: C:\Users\SLINK-004\src\ch2\bingocard\src\main.rs
--- ch2/dice/target/debug/deps/dice.d
+++
@@ -1,5 +0,0 @@
-C:\Users\SLINK-004\src\ch2\dice\target\debug\deps\dice.exe: src\main.rs
-
-C:\Users\SLINK-004\src\ch2\dice\target\debug\deps\dice.d: src\main.rs
-
-src\main.rs:
